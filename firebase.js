b2e1b8bd82c89ffd78b7f6560b83e212d1032745
VIIIbyuL <myjjk03@gmail.com>
2024-01-15 00:02:29 -0500

    Update Firebase authentication and add async storage dependency

--- firebase.js
+++ firebase.js
@@ -1,12 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAv6-yKHTIrv-LKHjvmFqEWvavpH8DiF4Y",
   authDomain: "qrgen-67081.firebaseapp.com",
@@ -17,6 +14,12 @@ const firebaseConfig = {
   measurementId: "G-5NYCTKS00F",
 };
 
-const app = initializeApp(firebaseConfig); // Initialize Firebase
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app); // Initialize Firebase Authentication
+// Initialize Firebase Authentication with persistence
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+export { auth };
